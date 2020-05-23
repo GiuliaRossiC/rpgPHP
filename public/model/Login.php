42a4c273a53a50cbc7dd652d2cbd043eaa455e8c
carolinacinque <ccinque2@gmail.com>
2020-05-23 20:38:34 -0300

    Login

--- public/model/Login.php
+++ public/model/Login.php
@@ -6,16 +6,30 @@ class Login
     public static function estaLogado()
     {
         // verificar $_SESSION['usuario']
+        if(isset($_SESSION['usuario'])){
+            return true;
+        }
+        return false;
     }
 
     public static function login($email, $senha)
     {
+        $usuario = Usuario::verificarLogin($email, $senha);
+        if(is_null($usuario)){
+            header('Location: cadastro.php');
+        }else{
+            session_start();
+            $_SESSION['usuario'] = $usuario;
+            header('Location: servico.php');
+        }
         // usar Usuario::LoginValido e gravar $_SESSION['usuario']
     }
 
     public static function logout()
     {
         // apagar o $_SESSION['usuario']
+        session_start();
+        unset($_SESSION['usuario']);
     }
 
 }
