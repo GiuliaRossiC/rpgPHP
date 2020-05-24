be096e77735983f2671a9955668e42fcd23e9069
Ellyzinea <nancielly100@gmail.com>
2020-05-24 18:47:59 -0300

    Usuario.php Array de usuários

--- public/model/Usuario.php
+++ public/model/Usuario.php
@@ -1,17 +1,1 @@
 <?php
-// CRUD do usuario
-
-class Usuario
-{
-    public function verificarLogin($email, $senha)
-    {
-        //usar data::load(usuario) e verificar se login e senha existe
-    }
-
-    public function save($usuario, $senha)
-    {
-        // usar data::load(usuario) verificar se existe e usar data::save(usuario)
-        // $data = new data;
-        // $data-> save(usuario);
-    }
-}
